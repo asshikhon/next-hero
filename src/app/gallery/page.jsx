a5dce87a587870bf80b89cf725719123b1bbf1f1
asshikhon <asshikhon@gmail.com>
2024-06-27 20:55:54 +0600

    added some words

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -5,7 +5,7 @@ const page = () => {
     return (
         <div className='min-h-screen px-12 py-24'>
             <h6 className='text-3xl mb-12 text-center'>
-Gallery page
+Gallery pages
             </h6>
             <div>
             <Image src="/images/1.jpg.jpeg" alt="" height="1080" width="1920" />
